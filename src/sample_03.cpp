bcb15e4d1176d5aee99dc7a1021b50ddb25f810e
bumble <bumble.zhou@gmail.com>
2024-08-01 17:26:13 +0800

    co_wait && simplify generator

--- src/sample_03.cpp
+++ src/sample_03.cpp
@@ -6,104 +6,146 @@
 #include <cmath>
 #include <memory>
  
-template<typename T>
-struct Generator
-{
-  // The class name 'Generator' is our choice and it is not required for coroutine
-  // magic. Compiler recognizes coroutine by the presence of 'co_yield' keyword.
-  // You can use name 'MyGenerator' (or any other name) instead as long as you include
-  // nested struct promise_type with 'MyGenerator get_return_object()' method.
-  // (Note: It is necessary to adjust the declarations of constructors and destructors
-  //  when renaming.)
+// template<typename T>
+// struct Generator
+// {
+//   // The class name 'Generator' is our choice and it is not required for coroutine
+//   // magic. Compiler recognizes coroutine by the presence of 'co_yield' keyword.
+//   // You can use name 'MyGenerator' (or any other name) instead as long as you include
+//   // nested struct promise_type with 'MyGenerator get_return_object()' method.
+//   // (Note: It is necessary to adjust the declarations of constructors and destructors
+//   //  when renaming.)
 
+//   struct promise_type;
+//   using handle_type = std::coroutine_handle<promise_type>;
+
+//   struct promise_type // required
+//   {
+//     T value_;
+//     std::exception_ptr exception_;
+
+//     Generator get_return_object()
+//     {
+//       std::cout << "Generator -> promise_type -> get_return_object(value:" << value_ << ")" << "\n";
+//       return Generator(handle_type::from_promise(*this));
+//     }
+//     std::suspend_always initial_suspend() { return {}; }
+//     std::suspend_always final_suspend() noexcept { return {}; }
+//     void unhandled_exception() { exception_ = std::current_exception(); } // saving exception
+
+//     template<std::convertible_to<T> From> // C++20 concept
+//     std::suspend_always yield_value(From&& from)
+//     {
+//       std::cout << "Generator -> promise_type -> yield_value(value:" << value_ << ", from: " << std::forward<From>(from) << ")" << "\n";
+//       value_ = std::forward<From>(from); // caching the result in promise
+//       return {};
+//     }
+//     void return_void() {}
+//   };
+
+//   handle_type h_;
+
+//   Generator(handle_type h) : h_(h) {
+//     std::cout << "Generator constructor" << "\n";
+//   }
+//   ~Generator() { 
+//     h_.destroy();
+//     std::cout << "Generator destructor" << "\n";
+//   }
+//   explicit operator bool()
+//   {
+//     std::cout << "Generator -> Before explicit operator bool() ==> value: " << h_.promise().value_ << ", " << "full_: " << full_ << " ... " << "\n";
+//     std::cout << "fill() \n";
+//     fill(); // The only way to reliably find out whether or not we finished coroutine,
+//             // whether or not there is going to be a next value generated (co_yield)
+//             // in coroutine via C++ getter (operator () below) is to execute/resume
+//             // coroutine until the next co_yield point (or let it fall off end).
+//             // Then we store/cache result in promise to allow getter (operator() below
+//             // to grab it without executing coroutine).
+//     std::cout << "Generator -> After  explicit operator bool() ==> value: " << h_.promise().value_ << ", " << "full_: " << full_ << " ... " << "\n";
+//     return !h_.done();
+//   }
+//   T operator()()
+//   {
+//     std::cout << "Generator -> Before T operator()() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
+//     std::cout << "fill() \n";
+//     fill();
+//     std::cout << "full_ = false \n";
+//     full_ = false; // we are going to move out previously cached result to make promise empty again
+//     std::cout << "return std::move(h_.promise().value_) \n";
+//     std::cout << "Generator -> After  T operator()() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
+//     return std::move(h_.promise().value_);
+//   }
+ 
+// private:
+//   bool full_ = false;
+
+//   void fill()
+//   {
+//     std::cout << "Generator -> Before fill() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
+//     if (!full_)
+//     {
+//       std::cout << "h_() \n";
+//       h_();
+//       if (h_.promise().exception_)
+//       {
+//         std::rethrow_exception(h_.promise().exception_);
+//       }
+//       // propagate coroutine exception in called context
+//       std::cout << "full_ = true \n";
+//       full_ = true;
+//     }
+//     std::cout << "Generator -> After  fill() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
+//   }
+// };
+
+template <typename T>
+class Generator {
+public:
   struct promise_type;
-  using handle_type = std::coroutine_handle<promise_type>;
-
-  struct promise_type // required
-  {
-    T value_;
-    std::exception_ptr exception_;
-
-    Generator get_return_object()
-    {
-      std::cout << "Generator -> promise_type -> get_return_object(value:" << value_ << ")" << "\n";
-      return Generator(handle_type::from_promise(*this));
+  using promise_handle_t = std::coroutine_handle<promise_type>;
+  explicit Generator(promise_handle_t h) : handle_(h) {}
+  ~Generator() {
+    if (handle_) {
+      handle_.destroy();
     }
-    std::suspend_always initial_suspend() { return {}; }
-    std::suspend_always final_suspend() noexcept { return {}; }
-    void unhandled_exception() { exception_ = std::current_exception(); } // saving exception
+  }
+  Generator(Generator &) = delete;
+  Generator &operator=(Generator &) = delete;
 
-    template<std::convertible_to<T> From> // C++20 concept
-    std::suspend_always yield_value(From&& from)
-    {
-      std::cout << "Generator -> promise_type -> yield_value(value:" << value_ << ", from: " << std::forward<From>(from) << ")" << "\n";
-      value_ = std::forward<From>(from); // caching the result in promise
+  struct promise_type {
+    Generator get_return_object() {
+      return Generator{std::coroutine_handle<promise_type>::from_promise(*this)};
+    }
+    std::suspend_always initial_suspend() noexcept { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    void unhandled_exception() {}
+    void return_value(T t) noexcept {
+     v = t;
+    }
+    std::suspend_always yield_value(T t) {
+      v = t;
       return {};
     }
-    void return_void() {}
+    // void return_void() {}
+    T v{};
   };
-
-  handle_type h_;
-
-  Generator(handle_type h) : h_(h) {
-    std::cout << "Generator constructor" << "\n";
+  explicit operator bool() {
+    return !handle_.done();
   }
-  ~Generator() { 
-    h_.destroy();
-    std::cout << "Generator destructor" << "\n";
+  T operator ()() {
+    handle_.resume();
+    return handle_.promise().v;
   }
-  explicit operator bool()
-  {
-    std::cout << "Generator -> Before explicit operator bool() ==> value: " << h_.promise().value_ << ", " << "full_: " << full_ << " ... " << "\n";
-    std::cout << "fill() \n";
-    fill(); // The only way to reliably find out whether or not we finished coroutine,
-            // whether or not there is going to be a next value generated (co_yield)
-            // in coroutine via C++ getter (operator () below) is to execute/resume
-            // coroutine until the next co_yield point (or let it fall off end).
-            // Then we store/cache result in promise to allow getter (operator() below
-            // to grab it without executing coroutine).
-    std::cout << "Generator -> After  explicit operator bool() ==> value: " << h_.promise().value_ << ", " << "full_: " << full_ << " ... " << "\n";
-    return !h_.done();
-  }
-  T operator()()
-  {
-    std::cout << "Generator -> Before T operator()() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
-    std::cout << "fill() \n";
-    fill();
-    std::cout << "full_ = false \n";
-    full_ = false; // we are going to move out previously cached result to make promise empty again
-    std::cout << "return std::move(h_.promise().value_) \n";
-    std::cout << "Generator -> After  T operator()() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
-    return std::move(h_.promise().value_);
-  }
- 
 private:
-  bool full_ = false;
-
-  void fill()
-  {
-    std::cout << "Generator -> Before fill() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
-    if (!full_)
-    {
-      std::cout << "h_() \n";
-      h_();
-      if (h_.promise().exception_)
-      {
-        std::rethrow_exception(h_.promise().exception_);
-      }
-      // propagate coroutine exception in called context
-      std::cout << "full_ = true \n";
-      full_ = true;
-    }
-    std::cout << "Generator -> After  fill() ==> full: " << full_ << ", value: " << h_.promise().value_ << "\n";
-  }
+  std::coroutine_handle<promise_type> handle_;
 };
  
 Generator<std::uint64_t>
 fibonacci_sequence(uint32_t n)
 {
   if (n == 0)
-    co_return;
+    co_return 0;
 
   if (n > 94)
     throw std::runtime_error("Too big Fibonacci sequence. Elements would overflow.");
@@ -113,14 +155,14 @@ fibonacci_sequence(uint32_t n)
 
   if (n == 1) {
     std::cout << "fibonacci_sequence(" << n << ") -> co_return " << "\n";
-    co_return;
+    co_return 0;
   }
 
   std::cout << "fibonacci_sequence(" << n << ") -> co_yield 1 " << "\n";
   co_yield 1;
 
   if (n == 2)
-    co_return;
+    co_return 0;
 
   std::uint64_t a = 0;
   std::uint64_t b = 1;
@@ -147,11 +189,8 @@ newton_method_for_pi(double_t initial_guess) {
     double_t x1 = x - (fx / fpx);
 
     if (std::abs(x1 - x) < epsilon) {
-      // return x1;
       std::cout << "############co_yield " << std::setprecision(15) << x1 << ";#############\n";
-      co_yield x1;
-      std::cout << "break; \n";
-      break;
+      co_return x1;
     }
 
     x = x1;
---
+++ src/sample_04.cpp
@@ -0,0 +1,68 @@
+#include <coroutine>
+#include <iostream>
+#include <stdexcept>
+#include <thread>
+ 
+auto switch_to_new_thread(std::jthread& out)
+{
+  struct awaitable
+  {
+    std::jthread* p_out;
+    bool await_ready() { return false; }
+    void await_suspend(std::coroutine_handle<> h)
+    {
+      std::jthread& out = *p_out;
+      if (out.joinable())
+      {
+        throw std::runtime_error("Output jthread parameter not empty");
+      }
+      out = std::jthread([h] { h.resume(); });
+      // Potential undefined behavior: accessing potentially destroyed *this
+      // std::cout << "New thread ID: " << p_out->get_id() << '\n';
+      std::cout << "await_suspend() New thread ID: " << out.get_id() << '\n'; // this is OK
+    }
+    void await_resume()
+    {
+      std::cout << "await_resume() New thread ID: " << p_out->get_id() << '\n';
+    }
+  };
+  return awaitable{&out};
+}
+ 
+struct task
+{
+    struct promise_type
+    {
+      task get_return_object()
+      {
+        std::cout << "get_return_object()\n";
+        return {}; 
+      }
+      std::suspend_never initial_suspend() { return {}; }
+      std::suspend_never final_suspend() noexcept { return {}; }
+      void return_void() {}
+      void unhandled_exception() {}
+    };
+};
+ 
+task resuming_on_new_thread(std::jthread& out)
+{
+    std::cout << "Coroutine started on thread: " << std::this_thread::get_id() << '\n';
+    co_await switch_to_new_thread(out);
+    // awaiter destroyed here
+    std::cout << "Coroutine resumed on thread: " << std::this_thread::get_id() << '\n';
+}
+ 
+int main()
+{
+  std::cout << "main start on thread: " << std::this_thread::get_id() << '\n';
+
+  std::jthread out;
+  resuming_on_new_thread(out);
+
+  std::cout << "main sleep for 100 milliseconds\n";
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  std::cout << "main end on thread: " << std::this_thread::get_id() << '\n';
+  return 0;
+}
