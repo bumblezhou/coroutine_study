d24a9effdf2a5b8c0e60512d15a177f328b45eaf
bumble <bumble.zhou@gmail.com>
2024-08-02 09:08:14 +0800

    awaitable and coroutine

--- src/sample_04.cpp
+++ src/sample_04.cpp
@@ -8,7 +8,11 @@ auto switch_to_new_thread(std::jthread& out)
   struct awaitable
   {
     std::jthread* p_out;
-    bool await_ready() { return false; }
+    bool await_ready()
+    {
+      std::cout << "await_ready() return false; \n";
+      return false;
+    }
     void await_suspend(std::coroutine_handle<> h)
     {
       std::jthread& out = *p_out;
@@ -16,7 +20,10 @@ auto switch_to_new_thread(std::jthread& out)
       {
         throw std::runtime_error("Output jthread parameter not empty");
       }
-      out = std::jthread([h] { h.resume(); });
+      out = std::jthread([h] {
+        std::cout << "jthread is running, thread ID: " << std::this_thread::get_id() << "\n";
+        h.resume();
+      });
       // Potential undefined behavior: accessing potentially destroyed *this
       // std::cout << "New thread ID: " << p_out->get_id() << '\n';
       std::cout << "await_suspend() New thread ID: " << out.get_id() << '\n'; // this is OK
@@ -31,18 +38,32 @@ auto switch_to_new_thread(std::jthread& out)
  
 struct task
 {
-    struct promise_type
+  struct promise_type
+  {
+    task get_return_object()
     {
-      task get_return_object()
-      {
-        std::cout << "get_return_object()\n";
-        return {}; 
-      }
-      std::suspend_never initial_suspend() { return {}; }
-      std::suspend_never final_suspend() noexcept { return {}; }
-      void return_void() {}
-      void unhandled_exception() {}
-    };
+      std::cout << "provise_type.get_return_object()\n";
+      return {}; 
+    }
+    std::suspend_never initial_suspend()
+    {
+      std::cout << "provise_type.initial_suspend()\n";
+      return {};
+    }
+    std::suspend_never final_suspend() noexcept
+    {
+      std::cout << "promise_type.final_suspend()\n";
+      return {};
+    }
+    void return_void()
+    {
+      std::cout << "promise_type.return_void()\n";
+    }
+    void unhandled_exception()
+    {
+      std::cout << "promise_type.unhandled_exception()\n";
+    }
+  };
 };
  
 task resuming_on_new_thread(std::jthread& out)
